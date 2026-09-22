ffaa5bf2c0ea5382847f749fa91f82d4a443a42b
tribunal7 <ryanangel734@gmail.com>
2026-09-22 13:11:16 -0400

    Align Netlify form detection with quote fields

--- app/form-detection/page.tsx
+++ app/form-detection/page.tsx
@@ -1,1 +1,17 @@
-export default function Detection(){return <div style={{display:'none'}}><form name="falcor-quote" data-netlify="true"><input name="name"/><input name="company"/><input name="email"/><input name="phone"/><input name="project-type"/><select name="product"><option value="">Select a product</option></select><input name="square-footage"/><input name="zip"/><input name="timeline"/><textarea name="message"/></form></div>}
+export default function Detection(){
+  return <div style={{display:'none'}}>
+    <form name="falcor-quote" data-netlify="true">
+      <input name="first-name"/>
+      <input name="last-name"/>
+      <input name="company"/>
+      <input name="email"/>
+      <input name="phone"/>
+      <input name="project-type"/>
+      <input name="product"/>
+      <input name="square-footage"/>
+      <input name="zip"/>
+      <input name="timeline"/>
+      <textarea name="message"/>
+    </form>
+  </div>
+}
