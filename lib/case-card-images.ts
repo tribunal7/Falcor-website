b8a234bd8325249f61cda482d174a2ede9444423
tribunal7 <ryanangel734@gmail.com>
2026-09-24 22:04:34 -0400

    Assemble case study card images

---
+++ lib/case-card-images.ts
@@ -0,0 +1,21 @@
+import {caseCardImagesPart1} from './case-card-images-1';
+import {caseCardImagesPart2} from './case-card-images-2';
+import {caseCardImagesPart3} from './case-card-images-3';
+import {caseCardImagesPart4} from './case-card-images-4';
+import {caseCardImagesPart5} from './case-card-images-5';
+import {caseCardImagesPart6} from './case-card-images-6';
+import {caseCardImagesPart7} from './case-card-images-7';
+import {caseCardImagesPart8} from './case-card-images-8';
+import {caseCardImagesPart9} from './case-card-images-9';
+
+export const caseCardImages: Record<string,string> = {
+  ...caseCardImagesPart1,
+  ...caseCardImagesPart2,
+  ...caseCardImagesPart3,
+  ...caseCardImagesPart4,
+  ...caseCardImagesPart5,
+  ...caseCardImagesPart6,
+  ...caseCardImagesPart7,
+  ...caseCardImagesPart8,
+  ...caseCardImagesPart9,
+};
